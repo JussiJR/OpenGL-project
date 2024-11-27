022919b2ee6c983d7cba289b58299dfc75cdffba
JussiJR <119566959+JussiJR@users.noreply.github.com>
2024-11-27 22:36:38 +0200

    added documentations
    added ebo document template into

--- WeeRun/WeeRun/VBO.cpp
+++ WeeRun/WeeRun/VBO.cpp
@@ -6,11 +6,11 @@ VBO::VBO()
 	ID = 0;
 }
 
-VBO::VBO(std::vector<Vertex>& vertices)
+VBO::VBO(std::vector<void*>& vertices)
 {
 	glGenBuffers(1, &ID);
 	glBindBuffer(GL_ARRAY_BUFFER, ID);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(void*), vertices.data(), GL_STATIC_DRAW);
 }
 
 void VBO::Bind()
